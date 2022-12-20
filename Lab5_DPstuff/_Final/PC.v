82957b86b6441c37f066bbb38429294a86e50de9
maybe vaggy <sarvagya6@gmail.com>
2022-12-20 05:40:10 -0500

    Update PC.v

--- Lab5_DPstuff/_Final/PC.v
+++ Lab5_DPstuff/_Final/PC.v
@@ -1,19 +1,11 @@
-module PC(addrin, addrout, clk, pcSel);
+module PC(addrin, addrout, clk);
 
 input [31:0] addrin; //32 bit address in
 input clk;
-input pcSel;
 output reg [31:0] addrout; //32 bit address out
 
 
 always@(posedge clk) begin
-if(pcSel == 1) begin
 	addrout <= addrin;
-end
-
-else begin
-	addrout <= addrout + 4;
-	end
-
 end 
 endmodule

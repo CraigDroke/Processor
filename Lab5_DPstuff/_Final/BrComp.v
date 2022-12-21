b742d1fa54ba65f5b5acd852d66c9882c5ad803a
Craig Droke <94210673+CraigDroke@users.noreply.github.com>
2022-12-21 15:02:59 -0500

    Update BrComp.v

--- Lab5_DPstuff/_Final/BrComp.v
+++ Lab5_DPstuff/_Final/BrComp.v
@@ -1,13 +1,13 @@
-module branch_comp(opA, opB, beq, blt, brUn);
+module BrComp(opA, opB, beq, blt, brUn);
 	input [31:0]opA, opB;
 	input brUn;
 	output reg beq, blt;
 	
 	always@(opA or opB or brUn)
 	begin
-		if (brUn == 1'b1) // so sanh co dau
+		if (brUn == 1'b1)
 		begin
-			if (opA[31] == opB[31]) // opA va opB cung bit dau
+			if (opA[31] == opB[31]) 
 			begin 
 				beq = (opA == opB); // beq = 1 neu A = B, beq = 0 neu A != B
 				blt = (opA < opB); 
@@ -18,7 +18,7 @@ module branch_comp(opA, opB, beq, blt, brUn);
 				blt = (opA[31] == 1); // opA = 1xxx..xxxx, opB = 0xxx..xxxx 
 			end
 		end
-		else // so sanh khong dau
+		else 
 		begin 
 			beq = (opA == opB); 
 			blt = (opA < opB);

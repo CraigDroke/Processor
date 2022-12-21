dc45a743b337fa6d062e2c624a25a7407bd3b400
maybe vaggy <sarvagya6@gmail.com>
2022-12-21 04:51:34 -0500

    updated code

--- Lab5_DPstuff/_Final/IMem.v
+++ Lab5_DPstuff/_Final/IMem.v
@@ -1,7 +1,10 @@
 module IMem (
     input [31:0] addr,
     input clk,
-    output [31:0] dout
+    output reg[31:0] dout,
+    output reg[4:0] rd_out,
+    output reg[4:0] rs1_out,
+    output reg[4:0] rs2_out
 );
     
     reg [7:0] mem [0:255];
@@ -50,11 +53,10 @@ module IMem (
 
         //read instructions
         dout <= {mem[addr + 3], mem[addr + 2], mem[addr + 1], mem[addr]};
+        rd_out <= dout[11:7];
+        rs1_out <= dout[19:15];
+        rs2_out <= dout[24:20];
 
-
-
-
-       
 
     end
     
--- Lab5_DPstuff/_Final/Immsinext.v
+++ Lab5_DPstuff/_Final/Immsinext.v
@@ -1,12 +1,33 @@
-module Immsinext(clk, in, out);
+module Immsinext(clk, in, imm_out, immSel);
 
-input [11:0] in;
+input [31:0] in;
 input clk;
-output reg [31:0] out;
+input immSel;
+output reg [31:0] imm_out;
 
 //broken code
 
+// always @(posedge clk) begin
+// 	out = in + 20'b00000000000000000000;
+// end
+
+//working code prolly
+
 always @(posedge clk) begin
-	out = in + 20'b00000000000000000000;
+	
+	if (immSel == 1'b1) begin
+	case (in[6:0])
+		7'b0010011: imm_out <= { {21{in[31]}}, in[30:25], in[24:21], in[20]};   // ADDI     -> I-Type
+        7'b0100011: imm_out <= { {21{in[31]}}, in[30:25], in[11:8], in[7]};     // SW       -> S-Type
+        7'b0010111: imm_out <= { in[31], in[30:20], in[19:12], {12{1'b0}} };             // AUIPC    -> U-Type
+        7'b0000011: imm_out <= { {21{in[31]}}, in[30:25], in[24:21], in[20]};   // LW       -> I-Type
+        7'b1101111: imm_out <= { {12{in[31]}}, in[19:12], in[20], in[30:25], in[24:21], {1{1'b0}}};  // JAL -> J-Type
+        7'b1100011: imm_out <= { {20{in[31]}}, in[7], in[30:25], in[11:8], {1{1'b0}}};  // BRANCH -> B-Type
+        default: imm_out <= 32'b0;
+	endcase
+	end
+	else begin
+		imm_out <= 32'b0;
+	end
 end
 endmodule
